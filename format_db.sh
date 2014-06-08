b24811df9b7eceb2d4b5b9cece15c4cb514e2033
Michele Carignani <michele.carignani@gmail.com>
2014-06-09 01:54:06 +0200

    fixed initialization script

--- format_db.sh
+++ format_db.sh
@@ -2,9 +2,7 @@
 # Usage: format_db.sh <user> <email>
 # The command prompts for password
 workon rn-django17
-echo "==> Creating super user $1"
-python manage.py createsuperuser --user $1 --email $2
 echo "==> Creating database"
 python manage.py migrate
 echo "==> Creating Quartiers, Storerooms, .."
-python stock_assign/initialization.py
+echo -e "from stock_assign.initialization import * \npopulate_db() \nquit()" | python manage.py shell
